6ac28848685639b1a8bf2416fbf84d3ed78cffa5
zl <zhang0226lin@163.com>
2022-09-22 18:24:02 +0800

    提交3-master

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("hello 1");
         System.out.println("hello 2");
+        System.out.println("hello 3 master");
     }
 
 }
